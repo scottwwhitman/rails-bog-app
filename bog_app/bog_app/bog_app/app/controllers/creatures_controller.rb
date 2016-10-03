2a745d95381d0bd5d5b7395c0503d16cf997c2f1
Scott Whitman <scott.w.whitman@gmail.com>
2016-10-02 18:14:34 -0700

    added edit and upate

--- bog_app/bog_app/bog_app/app/controllers/creatures_controller.rb
+++ bog_app/bog_app/bog_app/app/controllers/creatures_controller.rb
@@ -9,8 +9,6 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
-
     @creature = Creature.new(creature_params)
 
     if @creature.save
@@ -24,6 +22,26 @@ class CreaturesController < ApplicationController
     @creature = Creature.find_by_id(creature_id)
   end
 
+  def edit
+    creature_id = params[:id]
 
+    @creature = Creature.find_by_id(creature_id)
+  end
+
+  def update
+    creature_id = params[:id]
+
+    @creature = Creature.find_by_id(creature_id)
+
+    @creature.update_attributes(creature_params)
+
+    redirect_to creature_path(@creature)
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
 
 end
